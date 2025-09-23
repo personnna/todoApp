463b8d7ad42cb9e7111404985bc9b5f02d893f0d
personnna <kadyelyer@gmail.com>
2025-09-23 12:45:04 +0200

    Initial Commit

---
+++ effectivemobileTests/effectivemobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  effectivemobileTests.swift
+//  effectivemobileTests
+//
+//  Created by ellkaden on 23.09.2025.
+//
+
+import Testing
+@testable import effectivemobile
+
+struct effectivemobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
